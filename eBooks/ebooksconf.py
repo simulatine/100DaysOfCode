49f61ab0aca6e5eafc0bf4afe84293d91656f81b
Simulatine <simulatine@gmail.com>
2020-05-15 00:14:21 +0100

    Initial commit

---
+++ eBooks/ebooksconf.py
@@ -0,0 +1,29 @@
+#!python3
+# -*- coding: utf-8 -*-
+"""
+ebooksconf.py - Configuration parameters for eBooksConf
+"""
+
+CALIBRE_LIBRARY_LOCATION = (
+    "C://Users//Simulatine//Documents//Calibre Library//"
+)
+JSON_FILE = "./calibre_books.json"
+
+# My unique Airtable API Key
+# This should be kept secret and not shared.
+AIRTABLE_API_KEY = "********"
+
+# Unique Airtable database ID for my eBooks database.
+# The database ID is visible by going to AirTable online, opening the target
+# base, and selecting Help and then API Documentation.
+EBOOKS_DATABASE_ID = "********"
+
+CALIBRE_LIBRARY_LOCATION = (
+    "C://Users//Simulatine//Documents//Calibre Library//"
+)
+
+# List of Amazon My Kindle Content pages which have been saved to local disk
+AMAZON_CONTENT_PAGES = [
+    "Amazon_Content_Page_1.html",
+    "Amazon_Content_Page_2.html",
+]
