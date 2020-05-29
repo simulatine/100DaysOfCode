7efb580a7f8d42bdd6153c94353cc48fc449fbfe
Simulatine <simulatine@gmail.com>
2020-05-29 14:40:46 +0100

    Simple example from Making Games with Python & Pygame

---
+++ Pygame/drawing.py
@@ -0,0 +1,44 @@
+#!python3
+# -*- coding: utf-8 -*-
+
+import sys
+import pygame
+from pygame.locals import *
+
+# Set up some colors
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED =   (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE =  (  0,   0, 255)
+
+# Initialise Pygame, set up a window and fill it with white
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
+DISPLAYSURF.fill(WHITE)
+
+# Draw various shapes on the display surface
+pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
+pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
+pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
+pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
+pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
+
+# Run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
+
+pixObj = pygame.PixelArray(DISPLAYSURF)
+pixObj[480][380] = BLACK
+pixObj[482][382] = BLACK
+pixObj[484][384] = BLACK
+pixObj[486][386] = BLACK
+pixObj[488][388] = BLACK
+del pixObj
